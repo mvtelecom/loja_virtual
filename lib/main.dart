5b0de3138556fba59e3d492c3ef34c1f2aad1352
mv <mv.telecom20@gmail.com>
2020-06-21 22:25:30 -0300

    18 - Validando o email.

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/screens/base/base_screen.dart';
+
+void main() {
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lady Fashion',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 4, 125, 141),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
+        appBarTheme: const AppBarTheme(
+          elevation: 0
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BaseScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/base/base_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
+import 'package:lojavirtual/models/page_manager.dart';
+import 'package:lojavirtual/screens/login/login_screem.dart';
+import 'package:provider/provider.dart';
+
+class BaseScreen extends StatelessWidget {
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          LoginScreen(),
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+}
