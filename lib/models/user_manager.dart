cebcda20f34d670a3cf8b24f9bc321a4f5970ae4
mv <mv.telecom20@gmail.com>
2020-06-22 03:31:45 -0300

    27. Recuperando os Dados no Login.

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
@@ -10,7 +11,8 @@ class UserManager extends ChangeNotifier {
   }
 
   final FirebaseAuth auth = FirebaseAuth.instance;
-  FirebaseUser user;
+  final Firestore firestore = Firestore.instance;
+  User user;
   bool _loading = false;
 
   bool get loading => _loading;
@@ -21,7 +23,7 @@ class UserManager extends ChangeNotifier {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      this.user = result.user;
+      await _loadCurrentUser(firebaseUser: result.user);
 
       onSuccess();
     } on PlatformException catch (e) {
@@ -37,6 +39,7 @@ class UserManager extends ChangeNotifier {
           email: user.email, password: user.password);
 
       user.id = result.user.uid;
+      this.user = user;
       user.saveData();
 
       onSuccess();
@@ -51,11 +54,14 @@ class UserManager extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> _loadCurrentUser() async {
-    final FirebaseUser currentUser = await auth.currentUser();
+  Future<void> _loadCurrentUser({FirebaseUser firebaseUser}) async {
+    final FirebaseUser currentUser = firebaseUser ?? await auth.currentUser();
     if (currentUser != null) {
-      user = currentUser;
+      final DocumentSnapshot docUser =
+          await firestore.collection('users').document(currentUser.uid).get();
+      user = User.fromDocument(docUser);
+
+      notifyListeners();
     }
-    notifyListeners();
   }
 }
