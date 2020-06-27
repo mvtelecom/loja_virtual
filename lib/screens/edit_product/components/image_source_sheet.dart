ee8df3cfa25ec99b40786e9bcb3eb0513ff2718d
mv <mv.telecom20@gmail.com>
2020-06-27 11:59:27 -0300

    67. Exibindo CupertinoModalPopup Câmera/Galeria (iOS)

---
+++ lib/screens/edit_product/components/image_source_sheet.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImageSourceSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if(Platform.isAndroid){
+      return BottomSheet(
+        onClosing: (){},
+        builder: (_) => Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            FlatButton(
+              onPressed: (){
+
+              },
+              child: const Text('Câmera'),
+            ),
+            FlatButton(
+              onPressed: (){
+
+              },
+              child: const Text('Galeria'),
+            ),
+          ],
+        ),
+      );
+    }else{
+      return CupertinoActionSheet(
+        title: const Text('Selecionar foto para o item'),
+        message: const Text('Escolha a origem da foto'),
+        cancelButton: CupertinoActionSheetAction(
+          onPressed: Navigator.of(context).pop,
+          child: const Text('Cancelar'),
+        ),
+      );
+    }
+  }
+}
--- lib/screens/edit_product/components/images_form.dart
+++ lib/screens/edit_product/components/images_form.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/product.dart';
+import 'package:lojavirtual/screens/edit_product/components/image_source_sheet.dart';
 
 class ImagesForm extends StatelessWidget {
 
@@ -13,6 +17,59 @@ class ImagesForm extends StatelessWidget {
       initialValue: product.images,
       builder: (state){
 
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Carousel(
+            images: state.value.map<Widget>((image){
+              return Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  if(image is String)
+                    Image.network(image, fit: BoxFit.cover,)
+                  else
+                    Image.file(image as File, fit: BoxFit.cover,),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: Icon(Icons.remove),
+                      color: Colors.red,
+                      onPressed: (){
+                        state.value.remove(image);
+                        state.didChange(state.value);
+                      },
+                    ),
+                  )
+                ],
+              );
+            }).toList()..add(
+                Material(
+                  color: Colors.grey[100],
+                  child: IconButton(
+                    icon: Icon(Icons.add_a_photo),
+                    color: Theme.of(context).primaryColor,
+                    iconSize: 50,
+                    onPressed: (){
+                      if(Platform.isAndroid)
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (_) => ImageSourceSheet()
+                        );
+                      else
+                        showCupertinoModalPopup(
+                            context: context,
+                            builder: (_) => ImageSourceSheet()
+                        );
+                    },
+                  ),
+                )
+            ),
+            dotSize: 4,
+            dotSpacing: 15,
+            dotBgColor: Colors.transparent,
+            dotColor: Theme.of(context).primaryColor,
+            autoplay: false,
+          ),
+        );
       },
     );
   }
